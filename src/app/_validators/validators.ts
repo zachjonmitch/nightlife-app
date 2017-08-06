385ea20d6444632809b3bce45efb7ffe96376d79
Zach Mitchell <zjmitche@gmail.com>
2017-08-06 09:03:24 -0600

    Email login errors

---
+++ src/app/_validators/validators.ts
@@ -0,0 +1,21 @@
+import { FormGroup, FormControl } from '@angular/forms';
+
+export function emailValidator(control: FormControl): {[key: string]: any} {
+  var emailRegexp = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+  if (control.value && !emailRegexp.test(control.value)) {
+    return { invalidEmail: true };
+  }
+}
+
+export function matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+  return (group: FormGroup): {[key: string]: any} => {
+    let password = group.controls[passwordKey];
+    let confirmPassword = group.controls[confirmPasswordKey];
+    
+    if (password.value !== confirmPassword.value) {
+      return {
+        mismatchedPasswords: true
+      };
+    }
+  }
+}
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { emailValidator, matchingPasswords } from '../../_validators/validators';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -13,18 +15,23 @@ export class SignupComponent implements OnInit {
   email:string = '';
   username:string = '';
   password:string = '';
-  password2:string = '';
-  inputAlert:string = 'This field is required';
+  confirmPassword:string = '';
+  inputAlert:string = 'This field is required.';
+  usernameAlert:string = 'Usernames must be at least 3 characters.';
+  uniqueAlert:string = 'Username must be unique.';
+  emailAlert:string = 'Email is invalid.';
+  passwordAlert:string = 'Passwords must be at least 8 characters.';
+  matchAlert:string = 'Passwords do not match.';
+
 
   constructor(private fb: FormBuilder) {
 
     this.rForm = fb.group({
-      'email'    : [null, Validators.required],
-      'username' : [null, Validators.required],
-      'password' : [null, Validators.required],
-      'password2': [null, Validators.required],
-      'validate' : ''
-    })
+      'email'    : [null, Validators.compose([Validators.required, emailValidator])],
+      'username' : [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      'password' : [null, Validators.compose([Validators.required, Validators.minLength(8)])],
+      'confirmPassword': [null, Validators.required]
+    }, {validator: matchingPasswords('password', 'confirmPassword')})
   }
 
   ngOnInit() {
@@ -34,6 +41,8 @@ export class SignupComponent implements OnInit {
     this.email = post.email;
     this.username = post.username;
     this.password = post.password;
-    this.password2 = post.password2;
+    this.confirmPassword = post.confirmPassword;
+
+    console.log(this.rForm)
   }
 }
